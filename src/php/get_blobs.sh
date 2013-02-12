4f4838f7aac33e3b5319d2f287d282b5b5be691e
Stefan Schneider <stefan.schneider@uhurusoftware.com>
2013-02-12 12:08:36 +0200

    Added php

---
+++ src/php/get_blobs.sh
@@ -0,0 +1,19 @@
+# abort script on any command that exit with a non zero value
+set -e
+
+# from http://pcre.org/
+wget -nc ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.21.tar.gz
+
+wget -nc http://mirrors.hostingromania.ro/apache.org//httpd/httpd-2.4.3.tar.gz
+wget -nc http://mirrors.hostingromania.ro/apache.org//httpd/httpd-2.4.3-deps.tar.gz
+
+# from http://php.net
+wget -nc http://ro1.php.net/distributions/php-5.4.11.tar.gz
+
+
+
+wget -nc http://mirrors.hostingromania.ro/apache.org//httpd/httpd-2.2.23.tar.gz
+
+# from http://php.net
+wget -nc http://ro1.php.net/distributions/php-5.3.21.tar.gz
+
