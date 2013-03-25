346b284310bda27fe1d7d8bac78ffa4e22171c89
Stefan Schneider <stefan.schneider@uhurusoftware.com>
2013-03-25 17:13:26 +0200

    Added postgrs and cc audit

---
+++ jobs/cloud_controller/templates/enable_audit.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+require 'pg'
+
+config ||= {}
+
+config['ccdb'] =
+    {
+        'host' => ARGV[0],
+        'port' => ARGV[1],
+        'dbname' => ARGV[2],
+        'user' => ARGV[3],
+        'password' => ARGV[4],
+    }
+
+
+pg_conn = PGconn.connect(
+    host: config['ccdb']['host'],
+    port: config['ccdb']['port'],
+    dbname: config['ccdb']['dbname'],
+    user: config['ccdb']['user'],
+    password: config['ccdb']['password']
+)
+
+pg_conn.exec("SELECT audit.audit_table('users');")
+pg_conn.exec("SELECT audit.audit_table('apps');")
+pg_conn.exec("SELECT audit.audit_table('services');")
+pg_conn.exec("SELECT audit.audit_table('service_configs');")
+
